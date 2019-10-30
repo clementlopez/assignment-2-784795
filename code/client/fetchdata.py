d4cd7ced7fdf3133bf67a7165c14adfbe92f830c
clement.lopez <clement.lopez@moonshotlabs.com>
2019-10-30 22:02:35 +0200

    fetchdata is done

--- code/client/fetchdata.py
+++ code/client/fetchdata.py
@@ -1,35 +1,44 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import os
 try: #python3
     from urllib.request import urlopen
 except: #python2
     from urllib2 import urlopen
 import json
+import tarfile
+import docker
+import argparse
 
-fp = urlopen("http://localhost:5000/customer/customer-1")
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Init authentication')
+    #customer-X
+    parser.add_argument('-u', type=str, help='You must provide a userId')
+    #password-X
+    parser.add_argument('-p', type=str, help='You must fill in a String')
+    return parser.parse_args()
 
-encodedContent = fp.read()
-decodedContent = encodedContent.decode("utf8")
+if __name__ == "__main__":
+    args = parse_arguments()
 
-print(decodedContent)
-jsonContent = json.loads(decodedContent)
-
-nb_file_ingested = 0
-client_directory = os.getcwd()+"/client-input-dir"
-
-for file in os.listdir(client_directory):
-    if nb_file_ingested >= jsonContent["number_files"]:
-        print("The maximum number of files on the server is reached")
-        break
-    extension = os.path.splitext(file)[1]
-    if not extension in jsonContent["formats"]:
-        print("The extension of the file %s is not allowed" %(file))
-        continue
-    file_size = os.path.getsize(os.path.join(client_directory, file)) / 1024
-    if file_size > jsonContent["data_sizes"]:
-        print("The file %s is bigger than your maximum authorized size of %s KB" %(file, jsonContent["data_sizes"]))
-        continue
-    print("TODO envoyer file")
-
-
-fp.close()
+    client = docker.from_env()
+    fp = urlopen("http://localhost:5000/customer?id="+str(args.u)+"&password="+str(args.p))
+    encodedContent = fp.read()
+    decodedContent = encodedContent.decode("utf8")
+    jsonContent = json.loads(decodedContent)
+    nb_file_ingested = 0
+    client_directory = os.getcwd()+"/client-input-dir"
+    for file in os.listdir(client_directory):
+        if nb_file_ingested >= jsonContent["number_files"]:
+            print("The maximum number of files on the server is reached")
+            break
+        extension = os.path.splitext(file)[1]
+        if not extension in jsonContent["formats"]:
+            print("The extension of the file %s is not allowed" %(file))
+            continue
+        file_size = os.path.getsize(os.path.join(client_directory, file)) / 1024
+        if file_size > jsonContent["data_sizes"]:
+            print("The file %s is bigger than your maximum authorized size of %s KB" %(file, jsonContent["data_sizes"]))
+            continue
+        os.system("docker cp " + os.path.join(client_directory, file) + " server:/app/customer-1/"+file)
+        print("The file %s was uploaded successfully" %(file))
+    fp.close()
--- code/server/app/app.py
+++ code/server/app/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_restful import Resource, Api
 from json import dumps
 import json
+import hashlib, binascii, os
 app = Flask(__name__)
 api = Api(app)
 
@@ -11,14 +12,40 @@ api = Api(app)
 def hello():
     return render_template('index.html')
 
-class Customer_Profile(Resource):
-    def get(self, customer_id):
-        result = {}
-        with open(str(customer_id)+'/config.json') as json_data:
-            result = json.load(json_data)
-        return jsonify(result)
+def hash_password(password):
+    """Hash a password for storing."""
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), 
+                                salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
 
-api.add_resource(Customer_Profile, '/customer/<customer_id>') # Route_1
+def verify_password(stored_password, provided_password):
+    """Verify a stored password against one provided by user"""
+    salt = stored_password[:64]
+    stored_password = stored_password[64:]
+    pwdhash = hashlib.pbkdf2_hmac('sha512', 
+                                  provided_password.encode('utf-8'), 
+                                  salt.encode('ascii'), 
+                                  100000)
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return pwdhash == stored_password
+
+class Customer_Profile(Resource):
+    def get(self):
+        customer_id = request.args.get('id')
+        password = request.args.get('password')
+        with open('./users.json') as json_users:
+            authent = json.load(json_users)
+        if verify_password(authent[customer_id], password):
+            result = {}
+            print(password)
+            with open(str(customer_id)+'/config.json') as json_data:
+                result = json.load(json_data)
+            return jsonify(result)
+        return "Wrong id and/or password"
+
+api.add_resource(Customer_Profile, '/customer') # Route_1
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
