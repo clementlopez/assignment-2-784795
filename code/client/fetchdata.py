c2531ae19b329062eb61eea2a28e9b23fbada028
clement.lopez <clement.lopez@moonshotlabs.com>
2019-10-31 17:54:36 +0200

    part 1 completed

--- code/client/fetchdata.py
+++ code/client/fetchdata.py
@@ -15,11 +15,14 @@ def parse_arguments():
     parser.add_argument('-u', type=str, help='You must provide a userId')
     #password-X
     parser.add_argument('-p', type=str, help='You must fill in a String')
+    #dir-name
+    parser.add_argument('--d', type=str, help='Provide the client directory name', default='client-input-dir')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_arguments()
-
+    if args.u is None or args.p is None:
+        exit(0)
     client = docker.from_env()
     fp = urlopen("http://localhost:5000/customer?id="+str(args.u)+"&password="+str(args.p))
     encodedContent = fp.read()
@@ -29,7 +32,7 @@ if __name__ == "__main__":
     else:
         jsonContent = json.loads(decodedContent)
         nb_file_ingested = 0
-        client_directory = os.getcwd()+"/client-input-dir"
+        client_directory = os.getcwd()+"/"+args.d
         for file in os.listdir(client_directory):
             if nb_file_ingested >= jsonContent["number_files"]:
                 print("The maximum number of files on the server is reached")
@@ -43,6 +46,7 @@ if __name__ == "__main__":
                 print("The file %s is bigger than your maximum authorized size of %s KB" %(file, jsonContent["data_sizes"]))
                 continue
             # Upload on the server
-            os.system("docker cp " + os.path.join(client_directory, file) + " server:/app/customer-1/data/"+file)
+            os.system("docker cp " + os.path.join(client_directory, file) + " server:/app/"+str(args.u)+"/data/"+file)
             print("The file %s was uploaded successfully" %(file))
+            nb_file_ingested += 1
     fp.close()
